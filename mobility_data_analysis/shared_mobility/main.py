a0e975d28bc65f37fb26030b29af32b1ea93f7f7
unknown <ylvaleroc@gmail.com>
2023-04-04 17:58:11 +0200

    Translate English

--- mobility_data_analysis/shared_mobility/main.py
+++ mobility_data_analysis/shared_mobility/main.py
@@ -9,30 +9,30 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-"""
-# leer archivo CSV en fragmentos de 100,000 filas cada uno
+
+"""# Read CSV file in chunks of 100,000 rows each
 chunk_size = 100000
-i=0
+i = 0
 for chunk in pd.read_csv('data/Shared_Micromobility_Vehicle_Trips.csv', chunksize=chunk_size):
-    i+=1
+    i += 1
     break"""
 
-# Leer el archivo CSV
+# Read CSV file
 df = pd.read_csv('data/Shared_Micromobility_Vehicle_Trips.csv')
 
-# Convertir la columna 'Start Time' en un objeto de tipo fecha y hora
+# Convert 'Start Time' column to datetime object
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-# Extraer la fecha de cada viaje y guardarla en una nueva columna 'Date'
+# Extract date from each trip and store it in a new 'Date' column
 df['Date'] = df['Start Time'].dt.date
 
-# Crear una matriz origen-destino por día
+# Create an origin-destination matrix per day
 for date, group in df.groupby('Date'):
-    # Crear la matriz origen-destino
+    # Create origin-destination matrix
     trips = group.groupby(['Council District (Start)', 'Council District (End)']).size().reset_index(name='counts')
     matrix = pd.pivot_table(trips, values='counts', index=['Council District (Start)'], columns=['Council District (End)'], fill_value=0)
 
-    # Crear un gráfico de la matriz origen-destino
+    # Create a heatmap of the origin-destination matrix
     fig, ax = plt.subplots()
     im = ax.imshow(matrix, cmap='Blues', interpolation='nearest')
     ax.set_xticks(np.arange(len(matrix.columns)))
@@ -43,8 +43,17 @@ for date, group in df.groupby('Date'):
     for i in range(len(matrix.index)):
         for j in range(len(matrix.columns)):
             text = ax.text(j, i, matrix.iloc[i, j], ha="center", va="center", color="w")
-    ax.set_title('Matriz origen-destino el {}'.format(date))
-    plt.savefig('Resultados/{}.png'.format(date))
+    ax.set_title('Origin-Destination Matrix on {}'.format(date))
+    plt.savefig('Results/{}.png'.format(date))
+    plt.close(fig)
+
+    # Create a histogram of trip distances
+    fig, ax = plt.subplots()
+    ax.hist(group['Trip Distance'], bins=20, range=(0,20))
+    ax.set_xlabel('Trip Distance (miles)')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Distance Distribution on {}'.format(date))
+    plt.savefig('Distance/{}.png'.format(date))
     plt.close(fig)
 
     # Crear un gráfico de histograma de distancia recorrida
